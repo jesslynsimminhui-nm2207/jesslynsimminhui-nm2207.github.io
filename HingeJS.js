80a2f8e840d9418fc6fe0df0593746700215f351
jesslynsimminhui-nm2207 <125461273+jesslynsimminhui-nm2207@users.noreply.github.com>
2023-03-18 23:13:36 +0800

    W9 Upload

---
+++ HingeJS.js
@@ -0,0 +1,21 @@
+
+
+console.log("yo"); 
+
+window.onload = function() {
+
+    Array.from(document.getElementsByClassName("menu-items"))
+    .forEach((item,index) => {
+        item.onmouseover = () => {
+            menu.dataset.activeIndex = index;
+        }
+    });
+
+}
+const menu = document.getElementById("menu");
+
+
+
+
+console.log(Array.from(document.getElementsByClassName("menu-item")).length);
+
