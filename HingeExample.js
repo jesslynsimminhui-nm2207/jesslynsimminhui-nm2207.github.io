b75d1a9fc5edb8b948ea1db7488679b586f3e35a
jesslynsimminhui-nm2207 <125461273+jesslynsimminhui-nm2207@users.noreply.github.com>
2023-03-22 20:27:39 +0800

    Percentages

--- HingeExample.js
+++ HingeExample.js
@@ -1,22 +1,24 @@
 
 //PART 1: Overall Data
 var data = fetch("https://jesslynsimminhui-nm2207.github.io/matches.json").then(
-    (prom)=>(prom.json())
+    (prom) => (prom.json())
 ).then(
-    function(json) {
-        var stats = { 
+    function (json) {
+        var stats = {
             //interactions: 0, //size of json object
-            likes: 0,  
+            likes: 0,
             blocks: 0,
             matches: 0,
         }
 
-        var z ={
-            interactions: 0, 
-        }
+        //Separate Interactions because don't want it in the Chart 
+        var z = { interactions: 0,}
         z.interactions = json.length;
+
+        //ALL BACKEND AND MATH WORKS ------------------------------------------------------------
+        //Overall Chart Absolute Data
         for (var i = 0; i < json.length; i++) {
-           
+
             var obj = json[i];
             for (const item in obj) {
                 // console.log(item);
@@ -30,14 +32,21 @@ var data = fetch("https://jesslynsimminhui-nm2207.github.io/matches.json").then(
                     stats.matches++;
                 };
             
+
             }
         }
-
-        //console.log("So far: ");
+         //console.log("So far: ");
         //console.log(stats);
-        console.log(z);
+        //console.log(z);
 
-        //FORMATING THE TABLE 
+        //Percentage Chart Data 
+        const blockperc = Math.round((stats.blocks/z.interactions) * 100);
+        const likeperc = Math.round((stats.likes/z.interactions) * 100);
+        const matchperc = Math.round((stats.matches/z.interactions) * 100);
+       
+
+        // ALL THE DIFFERENT CHARTS BELOW ---------------------------------------------------------
+        //FORMATING THE OVERALL TABLE 
         var dataHeaders = [];
         var dataArray = [];
 
@@ -59,12 +68,12 @@ var data = fetch("https://jesslynsimminhui-nm2207.github.io/matches.json").then(
                     'rgb(255, 99, 132)',
                     'rgb(54, 162, 235, 0.8)',
                     'rgb(255, 205, 86)'
-                  ],
-                  hoverOffset: 4
-                }]
-            }
-        
-        
+                ],
+                hoverOffset: 4
+            }]
+        }
+
+
         new Chart("overallchart",
             {
                 type: 'doughnut',
@@ -74,114 +83,35 @@ var data = fetch("https://jesslynsimminhui-nm2207.github.io/matches.json").then(
                         display: true,
                         fontFamily: "Ibarra Real Nova",
                         fontSize: 24,
-                        fontColor: 'rgb(250, 244, 220)', 
+                        fontColor: 'rgb(250, 244, 220)',
                         text: ["In a Glance"],
                     },
                     legend: {
                         labels: {
-                            fontColor:'rgb(250, 244, 220)',
+                            fontColor: 'rgb(250, 244, 220)',
                         }
-                        
+
                     }
-                    
+
                 }
             }
         )
-
+        //HingeInteraction
         document.getElementById("HingeInteraction").innerHTML = "You've interacted with " + z.interactions + " people";
-    }
-);
+    },
 
 
-//PART 2: PERCENTAGE DATA
+)
 
-var data = fetch("https://jesslynsimminhui-nm2207.github.io/matches.json").then(
-    (prom)=>(prom.json())
-).then(
-    function(json) {
-        var stats = { 
-            //interactions: 0, //size of json object
-            likes: 0,  
-            blocks: 0,
-            matches: 0,
-        }
 
-        var z ={
-            interactions: 0, 
-        }
-        z.interactions = json.length;
-        for (var i = 0; i < json.length; i++) {
-           
-            var obj = json[i];
-            for (const item in obj) {
-                // console.log(item);
-                if (item == "block") {
-                    stats.blocks++;
-                };
-                if (item == "like") {
-                    stats.likes++;
-                };
-                if (item == "match") {
-                    stats.matches++;
-                };
-            
-            }
-        }
+//if (item == "block") {
+   // percentage(stats.blocks/stats.interactions)
+//};
+//if (item == "like") {
+  //  percentage(stats.likes/stats.interactions)
+//};
+//if (item == "match") {
+//    percentage(stats.matches/stats.interactions)
+//};
 
-        //console.log("So far: ");
-        //console.log(stats);
-        console.log(z);
-
-        //FORMATING THE TABLE 
-        var dataHeaders = [];
-        var dataArray = [];
-
-        for (const key of Object.keys(stats)) {
-            console.log(key);
-            dataHeaders.push(key);
-        }
-        for (const value of Object.values(stats)) {
-            console.log(value);
-            dataArray.push(value);
-        }
-
-        const dataObj = {
-            labels: dataHeaders,
-            datasets: [{
-                label: "My Hinge",
-                data: dataArray,
-                backgroundColor: [
-                    'rgb(255, 99, 132)',
-                    'rgb(54, 162, 235, 0.8)',
-                    'rgb(255, 205, 86)'
-                  ],
-                  hoverOffset: 4
-                }]
-            }
-        
-        
-        new Chart("overallchart",
-            {
-                type: 'doughnut',
-                data: dataObj,
-                options: {
-                    title: {
-                        display: true,
-                        fontFamily: "Ibarra Real Nova",
-                        fontSize: 24,
-                        fontColor: 'rgb(250, 244, 220)', 
-                        text: ["In a Glance"],
-                    },
-                    legend: {
-                        labels: {
-                            fontColor:'rgb(250, 244, 220)',
-                        }
-                        
-                    }
-                    
-                }
-            }
-        )
-
-    }
-);
+//console.log(percentage(stats.blocks/stats.interactions));
